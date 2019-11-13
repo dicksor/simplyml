a164efd7f1a737f07cf0502eb9dedafc307c2d22
Vincent Moulin <vincent.moulin@he-arc.ch>
2019-11-13 09:24:26 +0100

    new parser file

---
+++ parser.py
@@ -0,0 +1,36 @@
+import ply.lacc as yacc
+
+from lex import tokens
+import AST
+
+def p_programme_statement(p):
+    ''' programme : statement '''
+
+def p_programme_EOL(p):
+    ''' programme : statement EOL programme '''
+
+def p_statement(p):
+    ''' statement : assignation | structure '''
+
+
+def parse(program):
+    return yacc.parse(program)
+
+yacc.yacc(outputdir='generated')
+
+if __name__ == "__main__":
+    import sys 
+    	
+    prog = open(sys.argv[1]).read()
+    result = yacc.parse(prog)
+    if result:
+        print (result)
+            
+        import os
+        os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+        graph = result.makegraphicaltree()
+        name = os.path.splitext(sys.argv[1])[0]+'-ast.pdf'
+        graph.write_pdf(name) 
+        print ("wrote ast to", name)
+    else:
+        print ("Parsing returned no result!")
